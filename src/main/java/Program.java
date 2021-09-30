233920e6d33cc4fbe778f41dc9ae8d204195e78d
Akash Mehta <akashmehta10@gmail.com>
2021-09-29 21:43:29 -0500

    Commit for fixing unwanted dirs

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,15 +5,6 @@ public class Program {
         for(int i =0; i<10; i++) {
             ImgBeautify.generateImage("/Users/akashmehta/myImg" + i +" .png");
         }
-
-/*        ImgBeautify.generateImage("/Users/akashmehta/myImg2.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg3.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg4.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg5.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg6.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg7.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg8.png");
-        ImgBeautify.generateImage("/Users/akashmehta/myImg9.png");*/
         System.out.println("Image Generated");
     }
 }
